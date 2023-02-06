66d93631f69da017d334ae02974f5d505d8a6312
minzung <candy010425@gmail.com>
2023-02-06 21:09:09 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpabook/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpabook.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // Order 테이블에 있는 Member 필드에 의해 매핑
+    private List<Order> orders = new ArrayList<>();
+}
---
+++ src/main/java/jpabook/jpabook/domain/Order.java
@@ -0,0 +1,35 @@
+package jpabook.jpabook.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery; //배송정보
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+}
