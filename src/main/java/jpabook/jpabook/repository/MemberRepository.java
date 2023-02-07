ca4864d179125f1dce0517b4e0e724e0181ce836
minzung <candy010425@gmail.com>
2023-02-07 20:14:45 +0900

    회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpabook/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpabook.repository;
+
+import jpabook.jpabook.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // 스프링 빈 주입
+public class MemberRepository {
+
+    @PersistenceContext // EntityManager 주입
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findeOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findeAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name).getResultList();
+    }
+}
