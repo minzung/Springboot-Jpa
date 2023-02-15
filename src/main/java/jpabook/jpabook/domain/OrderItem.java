9fabceee9ece18b9b6264974ec08f7f2b4ade611
minzung <candy010425@gmail.com>
2023-02-15 20:14:25 +0900

    주문 도메인 개발
    주문 도메인 개발

--- src/main/java/jpabook/jpabook/domain/OrderItem.java
+++ src/main/java/jpabook/jpabook/domain/OrderItem.java
@@ -1,13 +1,18 @@
 package jpabook.jpabook.domain;
 
 import jpabook.jpabook.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // protected OrderItem(){}을  롬복으로
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +29,26 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 주문 수량
+
+
+    //==생성 메서드==//
+    public static OrderItem createOrder(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
